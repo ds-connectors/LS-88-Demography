a729e188e428f806f1dfe5c45601079c9a17f64f
dennis feehan <feehan@berkeley.edu>
2018-11-05 11:42:58 -0800

    added lab08

---
+++ lab08/tests/test_TEMPLATE.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'type': 'wwpp',
+      'cases': [
+        {
+          'code': r"""
+          >>> True
+          True
+          """,
+          'hidden': False,
+        }         
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}

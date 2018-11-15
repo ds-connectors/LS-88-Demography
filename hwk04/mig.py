3d51a3b02b24aca8859eaf36119298d21745583d
dennis feehan <feehan@berkeley.edu>
2018-11-15 10:13:37 -0800

    one more update

--- hwk04/mig.py
+++ hwk04/mig.py
@@ -4,9 +4,10 @@ from datascience import *
 #import matplotlib
 #matplotlib.use('Agg')
 #import matplotlib.pyplot as plt
-#import numpy as np
-#import os
 #plt.style.use('fivethirtyeight')
+
+import numpy as np
+import os
 
 import pandas as pd
 import zipfile
